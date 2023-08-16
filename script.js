472909e6e4e068e47e266f58d997acef7d2749f5
Akash Gupta <113922776+akashgupta1905@users.noreply.github.com>
2023-08-16 21:46:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
-
 const person = {
   name: "John Doe",
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Create a reference to the 'players' array
+const team = players;
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+// Create a copy of the 'players' array
+const team1 = [...players];
+
+// Create a copy of the 'person' object
+const cap1 = { ...person };
+
+// Example usage and testing
+console.log("Original players:", players);
+console.log("Reference to players (team):", team);
+console.log("Copy of players (team1):", team1);
+console.log("Copy of person (cap1):", cap1);
+
+// Modifying team1 and cap1 to demonstrate independence
+team1.push("Sam");
+cap1.name = "Jane Smith";
+
+console.log("Modified team1:", team1);
+console.log("Modified cap1:", cap1);
+
